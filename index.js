4c1a417d3a92e34d0b93cb794691a9d776c21d63
K3be <ahmed.13.alkaaby@gmail.com>
2019-11-02 13:20:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+let map= function(arr,callback){
+  let newarr=[];
+  for (let i=0;i<arr.length;i++){
+    newarr.push(callback(arr[i]));
+  }
+  return newarr;
+}
+
+
+let reduce = function(arr,callback,startpoint=0){
+  let temp;
+  if(startpoint){
+    temp=startpoint
+  }
+  else
+  {temp=arr[0]}
+  let i;
+  if(startpoint){
+    i=0;
+  }
+  else{
+    i=1;
+  }
+  for(;i<arr.length;i++){
+    temp=callback(temp,arr[i])
+  }
+  return temp;
+}
